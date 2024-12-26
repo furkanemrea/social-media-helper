a12920e81d290e5b7e40c216ad14a65d51c6fbd6
FEA <furkanemrealtintas@gmail.com>
2024-12-26 12:55:38 +0100

    url fixed for proxy.

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -1,6 +1,6 @@
 export const convertImageToBase64 = async (imageUrl: string): Promise<string> => {
   try {
-    const response = await fetch(`http://13.60.250.114//convert-to-base64?url=${encodeURIComponent(imageUrl)}`);
+    const response = await fetch(`http://13.60.250.114:9898/convert-to-base64?url=${encodeURIComponent(imageUrl)}`);
     const data = await response.json();
     return "data:image/jpeg;base64,"+data.base64;
   } catch (error) {
