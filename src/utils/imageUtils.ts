bcf477f66b692af802997f90dcdf04813ace3aa8
FEA <furkanemrealtintas@gmail.com>
2024-12-26 12:54:21 +0100

    converturl fixed.

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -1,10 +1,10 @@
 export const convertImageToBase64 = async (imageUrl: string): Promise<string> => {
   try {
-    const response = await fetch(`http://localhost:9898/convert-to-base64?url=${encodeURIComponent(imageUrl)}`);
+    const response = await fetch(`http://13.60.250.114//convert-to-base64?url=${encodeURIComponent(imageUrl)}`);
     const data = await response.json();
     return "data:image/jpeg;base64,"+data.base64;
   } catch (error) {
-    console.error('Error converting image to base64:', error);
+    console.error('Error converting image to base64:', error);  
     throw error;
   }
 }; 
