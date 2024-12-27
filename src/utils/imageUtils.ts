53602b38d3e119b2e7554058d17c8268b569de42
FEA <furkanemrealtintas@gmail.com>
2024-12-27 20:12:28 +0100

    fix file

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -1,6 +1,6 @@
 export const convertImageToBase64 = async (imageUrl: string): Promise<string> => {
   try {
-    const response = await fetch(`http://ec2-13-60-250-114.eu-north-1.compute.amazonaws.com:9898/convert-to-base64?url=${encodeURIComponent(imageUrl)}`);
+    const response = await fetch(`https://peaceful-castle-92022-d494fb7e686f.herokuapp.com/convert-to-base64?url=${encodeURIComponent(imageUrl)}`);
     const data = await response.json();
 
     return "data:image/jpeg;base64,"+data.base64;
